7e93e34dfb52e6422cc04dd6c01f2d78102a4df1
Spencer McIntyre <Spencer_McIntyre@rapid7.com>
2022-05-12 16:21:20 -0400

    Override handle_intermediate_stage

--- lib/msf/core/payload/custom.rb
+++ lib/msf/core/payload/custom.rb
@@ -3,6 +3,29 @@
 #
 module Msf::Payload::Custom
 
+  def stage_payload(_opts = {})
+    print_line('Trying to stage Payload')
+    unless datastore['SHELLCODE_FILE'].nil?
+      shellcode = File.binread(datastore['SHELLCODE_FILE'])
+      if datastore['PrependSize']
+        return [ shellcode.length ].pack('V') + shellcode
+      else
+        return shellcode
+      end
+    end
+  end
+
+  def handle_intermediate_stage(conn, payload)
+    if( self.module_info['Stager']['RequiresMidstager'] == false ) && datastore['PrependSize']
+      print_status("Skipping sending the payload length a second time")
+      #conn.put( [ payload.length ].pack('V') )
+      # returning false allows stager.rb!handle_connection() to prepend the stage_prefix if needed
+      return false
+    end
+
+    super
+  end
+
   def read_stage_size?
     true
   end
