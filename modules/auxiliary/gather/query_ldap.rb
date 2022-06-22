a0ede1e23cfc6f849483dddfec2933755b8903d2
Grant Willcox <gwillcox@rapid7.com>
2022-06-22 14:30:28 -0500

    Add in table output to the module

--- modules/auxiliary/gather/query_ldap.rb
+++ modules/auxiliary/gather/query_ldap.rb
@@ -59,18 +59,18 @@ class MetasploitModule < Msf::Auxiliary
     ])
   end
 
-  def perform_ldap_query(ldap, filter, entries)
+  def perform_ldap_query(ldap, filter)
     returned_entries = ldap.search(base: @base_dn, filter: filter)
     if returned_entries.nil? || returned_entries.empty?
       print_error("No results found for #{filter}. You may require additional authentication, or the information may not exist on the target.")
-    elseSs
-      entries << [filter.to_s, returned_entries]
+      [nil, nil]
+    else
+      [filter.to_s, returned_entries]
     end
   end
 
   def run
-    entries = []
-
+    entries = nil
     begin
       ldap_connect do |ldap|
         if (@base_dn = datastore['BASE_DN'])
@@ -92,59 +92,58 @@ class MetasploitModule < Msf::Auxiliary
           print_status("Querying using #{datastore['LDAPQUERY']} on #{peer}")
           # Perform custom query
           filter = Net::LDAP::Filter.construct(datastore['LDAPQUERY'])
-          perform_ldap_query(ldap, filter, entries)
+          entries = perform_ldap_query(ldap, filter)
 
         # Many of the following queries came from http://www.ldapexplorer.com/en/manual/109050000-famous-filters.htm. All credit goes to them for these popular queries.
         when 'ENUM_ALL_OBJECTCLASS'
           filter = Net::LDAP::Filter.construct('(objectClass=*)') # Get ALL of the objects that have any objectClass associated with them. Can return a lot of info.
-          perform_ldap_query(ldap, filter, entries)
-
+          entries = perform_ldap_query(ldap, filter)
         when 'ENUM_ALL_OBJECTCATEGORY'
           filter = Net::LDAP::Filter.construct('(objectCategory=*)') # Get ALL of the objects that have any objectCategory associated with them. Can return a lot of info.
-          perform_ldap_query(ldap, filter, entries)
+          entries = perform_ldap_query(ldap, filter)
 
         when 'ENUM_COMPUTERS'
           filter = Net::LDAP::Filter.construct('(objectCategory=Computer)') # Find computers
-          perform_ldap_query(ldap, filter, entries)
+          entries = perform_ldap_query(ldap, filter)
 
         when 'ENUM_DOMAIN_CONTROLERS'
           filter = Net::LDAP::Filter.construct('(&(objectCategory=Computer)(userAccountControl:1.2.840.113556.1.4.803:=8192))') # Find domain controllers
-          perform_ldap_query(ldap, filter, entries)
+          entries = perform_ldap_query(ldap, filter)
 
         when 'ENUM_EXCHANGE_SERVERS'
           filter = Net::LDAP::Filter.construct('(&(objectClass=msExchExchangeServer)(!(objectClass=msExchExchangeServerPolicy)))') # Find Exchange Servers
-          perform_ldap_query(ldap, filter, entries)
+          entries = perform_ldap_query(ldap, filter)
 
         when 'ENUM_EXCHANGE_RECIPIENTS'
           # Find Exchange Recipients with or without fax addresses.
           filter = Net::LDAP::Filter.construct('(|(mailNickname=*)(proxyAddresses=FAX:*))')
-          perform_ldap_query(ldap, filter, entries)
+          entries = perform_ldap_query(ldap, filter)
 
         when 'ENUM_GROUPS'
           # Standard LDAP groups query, followed by trying to find AD security groups, then trying to find Linux groups.
           # Filters combined to remove duplicates.
           filter = Net::LDAP::Filter.construct('(|(objectClass=group)(objectClass=groupOfNames)(groupType:1.2.840.113556.1.4.803:=2147483648)(objectClass=posixGroup))')
-          perform_ldap_query(ldap, filter, entries)
+          entries = perform_ldap_query(ldap, filter)
 
         when 'ENUM_ORGUNITS'
           filter = Net::LDAP::Filter.construct('(objectClass=organizationalUnit)') # Find OUs aka Organizational Units
-          perform_ldap_query(ldap, filter, entries)
+          entries = perform_ldap_query(ldap, filter)
 
         when 'ENUM_ORGROLES'
           filter = Net::LDAP::Filter.construct('(objectClass=organizationalRole)') # Find OUs aka Organizational Units
-          perform_ldap_query(ldap, filter, entries)
+          entries = perform_ldap_query(ldap, filter)
 
         when 'ENUM_PEOPLE'
           filter = Net::LDAP::Filter.construct('(objectClass=organizationalPerson)') # Find people within an organization by Person entries.
-          perform_ldap_query(ldap, filter, entries)
-bcc
+          entries = perform_ldap_query(ldap, filter)
+
         when 'ENUM_USERS'
           # Common LDAP user query, followed by a query for AD User records by account type.
           # Finally, query for Linux accounts by objectClass.
           #
           # Doing this all in one query also prevents duplicate entires across multiple queries.
           filter = Net::LDAP::Filter.construct('(|(objectClass=inetOrgPerson)(objectClass=user)(sAMAccountType=805306368)(objectClass=posixAccount)(objectClass=GsAccount)(objectClass=GsSIPUser))')
-          perform_ldap_query(ldap, filter, entries)
+          entries = perform_ldap_query(ldap, filter)
         end
       end
     rescue Rex::ConnectionTimeout => e
@@ -152,7 +151,33 @@ bcc
       return
     end
 
-    p entries # XXX probably still need to improve output formatting here.
+    return if entries[1].nil?
+
+    columns = []
+    entries[1].each do |entry|
+      entry.attribute_names.each do |attribute|
+        columns << attribute.to_s
+      end
+    end
+    columns.uniq!
+    tbl = Rex::Text::Table.new(
+      'Header' => "#{action.name} Dump of #{peer}",
+      'Indent' => 1,
+      'Columns' => columns
+    )
+    entries[1].each do |entry|
+      data = []
+      columns.each do |col|
+        col = col.to_sym
+        if entry[col].nil? || entry[col].empty? || entry[col][0].empty?
+          data << nil
+        else
+          data << entry[col].join(' || ')
+        end
+      end
+      tbl << data
+    end
+    print_status(tbl.to_s)
   rescue Net::LDAP::Error => e
     print_error("#{e.class}: #{e.message}")
   end
